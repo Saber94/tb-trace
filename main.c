fdb3bfb7b5355a61b23c480f0042452cb6ecd22b
Saber94 <saberferjani@gmail.com>
2013-04-25 14:40:51 +0200

    sleep after overflush

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #define LINE_MAX 100
 #define CODE_GEN_MAX_BLOCKS 1024
-
+#define F_LENGTH 20
 
 #define ADRESS 		0
 #define SIZE 			1
@@ -14,7 +14,7 @@
 #define VALIDE 		5
 #define TRACE_ROWS 	6
 
-char filename[16];
+char filename[F_LENGTH];
 unsigned int nb_exec;
 unsigned int local_nb_exec;
 unsigned int nb_tran;
@@ -69,7 +69,7 @@ void Dump_Trace(char *filename)
    	{
    		Deviation = trace[i][NB_EXEC] - Esperance;
    		if (Deviation > 0) nb_pos_dev++;
-   		fprintf(f_trace,"%04u | %08x | %04u | %05u | %05u | %+5d | %5d | %u\n",
+   		fprintf(f_trace,"%04u | %08x | %04u | %05u | %05u | %+5d | %6d | %u\n",
    					i,
    					trace[i][ADRESS],
    					trace[i][SIZE],
@@ -113,11 +113,7 @@ int Lookup_tb(unsigned int Adress)
 	 { i++;	
 		if (i>nb_max_tb)
 		{
-			printf("\nTrace index Overflow! (i=%d ; loc_nb_tr=%d)\n",i,local_nb_tran);
-			snprintf(filename, sizeof(char) * 16, "Trace_Log.dat");
-			Dump_Trace(filename);
-
-			//exit(EXIT_FAILURE);
+			return -1;
 		}
 	 }
 	if (trace[i][ADRESS] == 0)
@@ -131,10 +127,11 @@ int Lookup_tb(unsigned int Adress)
 
 void flush(int quota)
 {
+	FILE *fdat;
    float ratio;
    char R_F = '_';
 	char L_M = '_';
-	if (Sim_mode!='1') 
+	if (Sim_mode!='1')
 	{
 		switch(Sim_mode) 
 		{
@@ -143,13 +140,15 @@ void flush(int quota)
 			case '4':  sort_row = 4; L_M = 'M';	R_F = 'R'; break;
 			case '5':  sort_row = 2; L_M = 'M';	R_F = 'F'; break;
 		}
-		snprintf(filename, sizeof(char) * 16, "Trace_%c%cU.dat",L_M ,R_F);
+		snprintf(filename, sizeof(char) * F_LENGTH, "Trace_%c%cU.dat",L_M ,R_F);
 		Dump_Trace(filename);
 	}
    nb_flush++;
 	tb_flushed = 1;
 	local_nb_tran = nb_tran - last_nb_tran;
 	local_nb_exec = nb_exec - last_nb_exec;
+	snprintf(filename, sizeof(char) * F_LENGTH, "trace/Trace_%u.dat", nb_flush);
+	Dump_Trace(filename);
 	qsort(trace, trace_size, TRACE_ROWS * sizeof(unsigned int), cmp);
 	trace_size = (nb_max_tb * quota)/32;
 	Trace_Init(trace_size);
@@ -170,13 +169,22 @@ void flush(int quota)
 	printf("nb cache hit             = %u\n",global_tb_hit);
 	ratio = ((float)global_tb_hit/nb_exec);
 	printf("cache_hit ratio          = %f\n",ratio);
+	fdat = fopen("hit_ratio.dat", "a");
+	if (fdat == NULL) 
+	{
+	printf("Couldn't open hit_ratio file for writing.\n");
+   exit(EXIT_FAILURE);
+   }
+   ratio = ((float)tb_hit / local_nb_exec);
+	fprintf(fdat, "%d, %f\n", nb_exec, ratio);
+   fclose(fdat);
 	last_nb_exec = nb_exec;
 	last_nb_tran = nb_tran;
 	tb_hit = 0;
 }
 
 /* ------------ Run simulation of cache policy by walking log file ------------ */
-void Run(FILE *f,unsigned int loop_exec, int quota, int threshold)
+void Run(FILE *f,unsigned int max_exec, int quota, int threshold)
 {
 	FILE *fdat;
 	int i;
@@ -185,7 +193,7 @@ void Run(FILE *f,unsigned int loop_exec, int quota, int threshold)
 
    float ratio;
 
-	if (tb_flushed) 
+	if (tb_flushed)
 	{
 		ratio = (float)local_nb_exec/nb_max_tb;
 		tb_flushed = 0;
@@ -201,13 +209,13 @@ void Run(FILE *f,unsigned int loop_exec, int quota, int threshold)
 
 	while (1)
    {
-   	if ((nb_exec > loop_exec) && loop_exec) 
-   		{printf("\nEnd of %d executions, please modify loop_exec to continue...\n",nb_exec);
-   		return;
-   		}
    	if (fgets(line,LINE_MAX,f)==NULL) 
    		{printf("\nEnd of trace file, Exiting... \n");
    		exit(EXIT_SUCCESS);
+   		}
+   	if ((nb_exec > max_exec) && max_exec) 
+   		{printf("\nEnd of %d executions, please modify max_exec to continue...\n",nb_exec);
+   		return;
    		}
    	if (next_line_is_adress) 
    		{next_line_is_adress=0;
@@ -218,25 +226,19 @@ void Run(FILE *f,unsigned int loop_exec, int quota, int threshold)
    		{switch(line[0]) {
       case 'I': next_line_is_adress = 1;														// In asm, next line is @
 					 nb_tran++;
-
-					 if ( ((nb_tran % threshold) == 0) && (Sim_mode != '1') )
+					 if ( (((nb_tran - last_nb_tran) % threshold) == 0) && (Sim_mode != '1') )
 					 {
-
 						flush(quota);
-						fdat = fopen("hit_ratio.dat", "a");
-      					if (fdat == NULL) {
-         					printf("Couldn't open hit_ratio file for writing.\n");
-        						exit(EXIT_FAILURE);
-      						}
-						fprintf(fdat, "%d, %f\n", nb_exec, ratio);
-    					fclose(fdat);
-			 	
 						return;
-					}
-
+					 }
 					break;
       case 'O': sscanf(line+11,"%u",&Read_Size);											// Out asm [size=%]
 					 i = Lookup_tb(Read_Adress);
+					 if (i == -1)
+					 	{
+					 	 printf("\ncache overflow..\n");
+						 exit(EXIT_FAILURE);
+						}
       			 trace[i][SIZE] = Read_Size;
       			 trace[i][NB_EXEC] = 0;
       			 trace[i][NB_TRANS]++;
@@ -245,16 +247,29 @@ void Run(FILE *f,unsigned int loop_exec, int quota, int threshold)
       case 'T': sscanf(line+22,"%x",&Read_Adress);											// Trace 
       			 printf("\rExecution   @ 0x%010x",Read_Adress);
       			 i = Lookup_tb(Read_Adress);
-					 if ((i<trace_size) && (trace[i][VALIDE])) 
-					 		{tb_hit++;}
+      			 if (i == -1)
+      			  {
+      			   flush(quota);
+      			   system("sleep 3");
+      			   i = Lookup_tb(Read_Adress);
+      			   if (trace[i][ADRESS] == 0)
+      			    {nb_tran++;
+      			    trace[i][ADRESS] = Read_Adress;
+      			    trace[i][NB_EXEC] = 0;
+      			 	 trace[i][NB_TRANS]++;
+      			 	 trace[i][VALIDE] = 0;
+      			 	 }
+      			  }
+					 if (trace[i][VALIDE])
+					 	{
+					 		tb_hit++;
+					 	}
       			 trace[i][NB_EXEC]++;
       			 trace[i][LAST_EXEC] = nb_exec;
 					 nb_exec++;
 					break;     
 		case 'F': if (Sim_mode == '1') 															// Qemu basic cache policy
 					{printf(line); 																	// tb_flush >> must return 
-   				 snprintf(filename, sizeof(char) * 16, "Trace_%u.dat", nb_flush);
-					 Dump_Trace(filename);
 					 flush(0);
 					return;
 					} 
@@ -295,12 +310,7 @@ char Simulation_Mode(char Sim_mode)
 	printf("4- Simulate MRU Cache Policy\n");
 	printf("5- Simulate MFU Cache Policy\n");
 	printf("0- Return\n");
-	do {read_char = getchar();} while((read_char!='1')  && 
-												 (read_char!='2')  && 
-												 (read_char!='3')  && 
-												 (read_char!='4')  &&
-												 (read_char!='5')  &&
-												 (read_char!='0'));
+	do {read_char = getchar();} while((read_char <'0') || (read_char >'5'));
 	if (read_char == '0') read_char = Sim_mode;
 	 switch(read_char) {
 		case '1': printf("Simulation mode : Qemu Basic Policy\n");break;	 	
@@ -315,7 +325,7 @@ char Simulation_Mode(char Sim_mode)
 /* ------------------------ Main program function ------------------------ */
 int main(int argc, char **argv)
 {
-	unsigned int loop_exec = 500000;
+	unsigned int max_exec = 500000;
 	int quota = 8;
 	int threshold;
 	FILE *f;
@@ -359,30 +369,21 @@ int main(int argc, char **argv)
 	printf("9 - Restart simulation\n");
 	printf("0 - Exit\n");  
 
-	do {read_char = getchar();} while((read_char !='1') && 
-												 (read_char !='2') && 
-												 (read_char !='3') && 
-												 (read_char !='4') && 
-												 (read_char !='5') && 
-												 (read_char !='6') && 
-												 (read_char !='7') &&
-												 (read_char !='8') && 
-												 (read_char !='9') &&
-												 (read_char !='0'));
+	do {read_char = getchar();} while((read_char <'0') || (read_char >'9'));
    switch(read_char) {
    	case '0': return;   	// exit program
-   	case '1': Run(f,loop_exec,quota,threshold);
+   	case '1': Run(f,max_exec,quota,threshold);
    					break;
-   	case '2': printf("Actual number of translations is %d, Enter new value (0 for continuous loop) : ",loop_exec);
-   				 scanf("%u",&loop_exec);
+   	case '2': printf("Actual number of translations is %d, Enter new value (0 for continuous loop) : ",max_exec);
+   				 scanf("%u",&max_exec);
    				 break;
    	case '3': do {printf("Actual Quota=%d/32, Enter new value : ",quota);
    					  scanf("%d/32",&quota);
-   					     threshold = nb_max_tb - (nb_max_tb*quota)/32; } 	while((quota != 1) && 
-   																								 	(quota != 2) &&
-   																									(quota != 4) &&
-   																									(quota != 8) &&
-   																									(quota != 16));
+   					  threshold = nb_max_tb - (nb_max_tb*quota)/32; } 	while((quota != 1) && 
+   																							 	(quota != 2) &&
+   																								(quota != 4) &&
+   																								(quota != 8) &&
+   																								(quota != 16));
    				 break;
    	case '4': do {printf("Actual Trace size = %d, Enter new value : ",nb_max_tb);
    					  scanf("%d",&nb_max_tb);
